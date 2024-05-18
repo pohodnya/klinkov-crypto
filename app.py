5f3b5967e6ecdb8bd844ffb6ce23d752e23c1f17
Aleksey <pohodnyaa@gmail.com>
2024-05-18 21:34:08 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import SocketServer
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+def some_function():
+    print "some_function got called"
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/test':
+            # Insert your code here
+            some_function()
+
+        self.send_response(200)
+
+httpd = SocketServer.TCPServer(("", 8080), MyHandler)
+httpd.serve_forever()
